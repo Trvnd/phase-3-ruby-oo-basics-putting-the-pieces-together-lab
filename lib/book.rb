0c71efc503060f6a6aa40241ed34bf928974c602
Jimmy Senecal <jamesnsenecal@gmail.com>
2022-06-13 16:20:32 -0400

    commit tree 8d7bc5933deb2d71be3c1e980bfa3c29080c87d6

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :title
 
+def initialize(title, author, page_count, genre)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+end
 
+end
+
+book = Book.new
+book.title = "And Then There Were None"
+book.title
